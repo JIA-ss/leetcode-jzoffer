41cb6944c229f9bfd9b485a417025ce22044c74d
Ss Jia <ssjia@SsdeMacBook-Pro.local>
2020-09-07 17:06:58 +0800

    2020/09/07

---
+++ review-各种排序算法/cmake-build-debug/CMakeFiles/review_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/review_.dir/main.cpp.o"
+  "review_"
+  "review_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/review_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ review-各种排序算法/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+/*
+ * 冒泡排序
+ * 比较相邻元素，如果左大，则交换
+ * */
+void maopao(int* a, int len) {
+    if (len == 0)   return;
+    for (int i = 0; i < len - 1; i++) {     //代表第i趟排序
+        for (int j = 0; j < len - 1- i; j++) {  //代表第j个元素
+            if (a[j] > a[j + 1])    swap(a[j],a[j+1]);
+        }
+    }
+}
+
+
+/*插入排序
+ * 前面都有序
+ * */
+void insert(int* a, int len) {
+    if (len == 0)   return ;
+    for (int i = 1; i < len; i++) {    //代表有序数组的长度
+        int num = a[i];     //待插入的元素
+        int j = i - 1;
+        for (j; j >= 0 && a[j] > num; j--) {  //从第j个开始，往前插入，
+            a[j + 1] = a[j];
+        }
+        a[j + 1] = num;
+    }
+}
+
+
+/*快速排序
+ * 1. 找到一个元素的位置，左边都比他小，右边都比他大
+ * 2. 左右分别递归
+ * */
+void quick(int* a, int start, int end) {
+    if (start >= end) return;
+    int i = start;
+    int j = end;
+    int temp = a[i];
+    while (i < j) {
+        while(i < j && a[j] >= temp)    j--;
+        while(i < j && a[i] <= temp)    i++;
+        swap(a[i],a[j]);
+    }
+    swap(a[i],a[start]);
+    quick(a,start,i-1);
+    quick(a,i+1,end);
+}
+
+
+int main() {
+    int a[8] = {4,5,6,7,3,22,2,1};
+    insert(a,8);
+    //quick(a,0,7);
+    for (int i = 0; i < 8; i++) cout << a[i] << " ";
+    return 0;
+}
