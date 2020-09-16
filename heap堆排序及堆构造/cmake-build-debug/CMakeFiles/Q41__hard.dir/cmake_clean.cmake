bd94278a0968b22fbfc78fa48b888d16346906f7
ssjia <627723154@qq.com>
2020-09-16 12:22:17 +0800

    2020/09/16

---
+++ heap堆排序及堆构造/cmake-build-debug/CMakeFiles/Q41__hard.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q41__hard.dir/main.cpp.o"
+  "Q41__hard"
+  "Q41__hard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Q41__hard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ heap堆排序及堆构造/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+void printf(int* a, string str) {
+    cout << str;
+    for(int i = 0; i < 10; i++) {
+        cout << a[i] <<" ";
+    }
+    cout << endl;
+}
+int i = 1;
+void heap_adjust_part(int* a,int root, int len) {
+    int lchild = root * 2 + 1;
+    int maxChild = lchild;
+    if (lchild < len) {
+        int rchild = lchild + 1;
+        if (rchild < len && a[rchild] > a[lchild])
+            maxChild = rchild;
+        if (a[maxChild] > a[root]) {    //堆被破坏，需要重新调整
+            swap(a[root], a[maxChild]);
+            printf(a,"第"+to_string(i++)+"次: ");
+            heap_adjust_part(a,maxChild,len);   //这里对于排序算法来说，可以注释掉，因为堆排序核心是要拿出最大的值，无需过于关心子树是否满足堆关系
+        }
+    }
+}
+
+void heap_init(int* a, int root, int len) {
+    for(int i = len / 2 - 1; i >= root; i--) {
+       heap_adjust_part(a,i,len);
+    }
+}
+
+void heap_sort(int* a, int len) {
+    //初始化堆 -> 遍历每个非叶子节点，进行一次局部的堆调整，可以让所有的最大值上浮，直至根节点 O(n)
+    heap_init(a,0,len);
+    swap(a[0],a[len-1]);
+    for(int i = 1; i < len; i++) {
+        //每次把最后的节点交换到根节点后，只需要进行局部调整即可
+        //每次局部调整是O(logn),相当于每一层调整一次
+        //一共遍历了n次，所以是O(nlogn)
+        heap_adjust_part(a,0,len - i);
+        swap(a[0],a[len-i-1]);
+    }
+    //total time   O(n) + O(nlogn) = O(nlogn)
+}
+
+
+int main(){
+    int a[10] = {112,112,107,128,121,118,105,113,106,116};
+    printf(a,"原始数组: ");
+    int len = 10;
+    heap_sort(a,len);
+    printf(a,"排序后: ");
+    //heap_adjust_tree(a,0,len);
+    //printf(a,"试一下：");
+    return 0;
+}
